3b541e574131b9981a3a1d659243561066d364b0
Ruben Eik Mortensen <202007594@post.au.dk>
2023-05-16 13:20:55 +0200

    Added customDataset

--- plotting/plottingSea.py
+++ plotting/plottingSea.py
@@ -41,15 +41,16 @@ path = os.path.realpath(__file__)
 dir = os.path.dirname(path)
 dir = dir.replace('plotting', 'trainingDatasets')
 os.chdir(dir)
-s= np.loadtxt('dataset12.txt').astype(np.int64)
-a = 1.2
+s= np.loadtxt('dataset10.txt').astype(np.int64)
+a = 1.0
 n = 10000000
 
 
 bincount = np.bincount(s)
 k = np.arange(1, s.max() + 1)
 plt.bar(k, bincount[1:], label='sample count')
-plt.plot(k, n*(k**-a)/zeta(a), 'k.-', label='expected count')   
+#plt.plot(k, n*(k**-a)/zeta(a), 'k.-', label='expected count')   
+plt.plot(k, n*(k**-a)/np.sum(k**-a), 'k.-', label='expected count')   
 plt.semilogy()
 plt.grid(alpha=0.4)
 plt.legend()
